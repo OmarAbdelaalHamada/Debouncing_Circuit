a68ce0d7a8258f74676c20754d2ee64837905e9e
omar <omark0635@gmail.com>
2024-08-13 13:35:50 +0300

    editing the synchronizer module

--- Synchronizer.v
+++ Synchronizer.v
@@ -2,4 +2,16 @@ module Synchronizer(clk,rst_n,noisy_in,sync_signal);
 parameter NUM_STAGES = 2;
 input clk,rst_n,noisy_in;
 output reg sync_signal;
+reg [NUM_STAGES-1:0]sync_register;
+always @(posedge clk or negedge rst_n) begin
+    if(~rst_n) begin 
+        sync_register <= 0;
+        sync_signal <= 0;
+    end
+    else begin
+        sync_register[0] <= noisy_in;
+        sync_register[1] <= sync_register[0];
+        sync_signal <= sync_register[1];
+    end
+end
 endmodule
