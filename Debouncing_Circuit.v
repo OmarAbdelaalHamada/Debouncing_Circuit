c18353a023db5e4e29a398f24b09268840497295
omar <omark0635@gmail.com>
2024-08-13 15:35:32 +0300

    editing the main module

--- Debouncing_Circuit.v
+++ Debouncing_Circuit.v
@@ -1,6 +1,10 @@
 module Debouncing_Circuit(clk,rst_n,noisy_in,debouncer_out);
 parameter NUM_STAGES = 2;
-parameter COUNTER_FINAL_VALUE = 20;
+parameter COUNTER_FINAL_VALUE = 100;
 input clk,rst_n,noisy_in;
 output reg debouncer_out;
+wire sync_signal,timer_done,timer_en;
+Synchronizer Sync(clk,rst_n,noisy_in,sync_signal);
+FSM fsm(clk,rst_n,sync_signal,timer_done,debouncer_out,timer_en);
+Timer timer(clk,rst_n,timer_en,timer_done);
 endmodule
